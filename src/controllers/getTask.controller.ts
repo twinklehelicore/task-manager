fe0f03df4ab474f1e2b4c77363c19aa9b9c3055d
Twinkle <twinklehelicore06@gmail.com>
2026-01-28 18:45:50 +0530

    deleted interface

--- src/controllers/getTask.controller.ts
+++ src/controllers/getTask.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import connection from "../db";
-import { Task } from "../interface/task.interface";
 
 export const getAllTasks = (req: Request, res: Response) => {
   let query = "SELECT * FROM tasks";
